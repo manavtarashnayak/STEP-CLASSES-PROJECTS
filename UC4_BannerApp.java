ef5035217de556cdd821c984d4224a49d366c764
Manav <manavtarashnayak@gmail.com>
2026-02-22 00:12:06 +0530

    update UC4 code

--- UC4_BannerApp.java
+++ UC4_BannerApp.java
@@ -1,11 +1,20 @@
-public class UC2_BannerApp {
+public class UC4_OOPS_Banner_Array {
+
     public static void main(String[] args) {
-        System.out.println("  ***    ***    *****   ***** ");
-        System.out.println(" *   *  *   *   *    *  *     ");
-        System.out.println("*     **     *  *    *  *     ");
-        System.out.println("*     **     *  *****   ***** ");
-        System.out.println("*     **     *  *           * ");
-        System.out.println(" *   *  *   *   *           * ");
-        System.out.println("  ***    ***    *       ***** ");
+
+        String[] banner = {
+            String.join("   ", " ***** ", " ***** ", " **** ", " ***** "),
+            String.join("   ", "*     *", "*     *", "*   *", "*     "),
+            String.join("   ", "*     *", "*     *", "**** ", " *** "),
+            String.join("   ", "*     *", "*     *", "*    ", "    *"),
+            String.join("   ", "*     *", "*     *", "*    ", "    *"),
+            String.join("   ", "*     *", "*     *", "*    ", "    *"),
+            String.join("   ", " ***** ", " ***** ", "*    ", " ***** ")
+        };
+
+        // Loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
