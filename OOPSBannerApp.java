e34b1fc493d975a5be2b0cdecaa0874e5cc33d6b
Manav <manavtarashnayak@gmail.com>
2026-02-23 02:01:16 +0530

    update dev

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,23 +1,4 @@
 public class OOPSBannerApp {
-<<<<<<< HEAD
-    public static void main(String[] args) {
-
-        String[] lines = new String[7];
-
-        lines[0] = "  ***    ***    *****   ***** ";
-        lines[1] = " *   *  *   *   *    *  *     ";
-        lines[2] = "*     **     *  *    *  *     ";
-        lines[3] = "*     **     *  *****   ***** ";
-        lines[4] = "*     **     *  *           * ";
-        lines[5] = " *   *  *   *   *           * ";
-        lines[6] = "  ***    ***    *       ***** ";
-
-        for (String line:lines) {
-            System.out.println(line);
-        }
-    }
-}
-=======
 
     public static void main(String[] args) {
 
@@ -36,4 +17,3 @@ public class OOPSBannerApp {
         }
     }
 }
->>>>>>> UC5-Array-Init
