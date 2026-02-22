9c3c02b1a7e90877f0462cbfe65ffddcecbd4400
Manav <manavtarashnayak@gmail.com>
2026-02-23 01:42:25 +0530

    update

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,4 +1,4 @@
-public class UC3_BannerApp {
+public class OOPSBannerApp {
     public static void main(String[] args) {
         System.out.println(String.join("","  ***    ***    *****   ***** "));
         System.out.println(String.join(""," *   *  *   *   *    *  *     "));
