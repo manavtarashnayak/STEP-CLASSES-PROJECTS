4b1ecd5a4f24d6538a7bcc2792aefcb988eb05bd
Manav <manavtarashnayak@gmail.com>
2026-02-23 01:15:59 +0530

    Update UC 2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,4 +1,4 @@
-public class UC2_BannerApp {
+public class OOPSBannerApp {
     public static void main(String[] args) {
         System.out.println("  ***    ***    *****   ***** ");
         System.out.println(" *   *  *   *   *    *  *     ");
